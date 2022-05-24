8e78a3295b4b070aee7abd9702641cbbcc266602
FelipeCdLima <felipe.lima@sptech.school>
2022-05-24 15:32:56 -0300

    Prot API 2.5

--- src/database/config.js
+++ src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "XXXXX",
-    database: "XXXXX",
-    user: "XXXXX",
-    password: "XXXXX",
+    server: "XXXX",
+    database: "XXXX",
+    user: "XXXX",
+    password: "XXXX",
     pool: {
         max: 10,
         min: 0,
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "SEU_DATABASE",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    database: "BLMConcept",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
